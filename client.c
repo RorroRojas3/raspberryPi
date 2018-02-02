00515645909d03bb5641e49ef4fcf8c134eadef9
Rodrigo Ignacio Rojas <rrojas@ullab10.ces.clemson.edu>
2018-02-02 14:03:15 -0500

    update client

--- client.c
+++ client.c
@@ -16,14 +16,14 @@
 
 int main(int argc, char *argv[])
 {
-	int clientSocket // Used to store the socket()
-	int numberOfBytes;	
+	int clientSocket; // Used to store the socket()
+	//int numberOfBytes;	
 	char buffer[MAXBYTES];	// Buffer
 	char serverIP_address[IPADDRESS];	// Will contain the Server's IP address
 	struct addrinfo clientInfo; // Will store the Host information
 	struct addrinfo *pClientInfo;	// Will point to Host information
 	int error = -1;	// Used as a error checking variable
-	char sentMessage[INET6_ADDRSTRLEN];	// Used to send messages to Server
+	//char sentMessage[INET6_ADDRSTRLEN];	// Used to send messages to Server
 	
 	if (argc != 2)
 	{
@@ -33,13 +33,13 @@ int main(int argc, char *argv[])
 	}	
 	else
 	{
-		strcpy(ip_address, argv[1]);
+		strcpy(serverIP_address, argv[1]);
 	}
 
 	memset(&clientInfo, 0, sizeof(clientInfo)); // Empties garbage from structure
 
 	clientInfo.ai_family = AF_UNSPEC; // IP can be either IPv4 or IPv6
-	clientInfo.ai_family = SOCK_STREAM // TCP connection
+	clientInfo.ai_socktype = SOCK_STREAM; // TCP connection
 	clientInfo.ai_flags = AI_PASSIVE; // Assigns Host IP address to socket structures
 
 	error = getaddrinfo(NULL, PORTNUMBER, &clientInfo, &pClientInfo); // pClientInfo points to clientInfo
@@ -53,15 +53,45 @@ int main(int argc, char *argv[])
 	if (clientSocket == -1)
 	{
 		printf("Error, socket failed\n");
+		free(pClientInfo);
 		exit(1);
 	}
 	
+	// connect(socket of the client, destination port and Host IP address, length in bytes of Server Address structure)
+	error = connect(clientSocket, pClientInfo->ai_addr, pClientInfo->ai_addrlen); // Connects to Server
+	if (error == -1)
+	{
+		printf("Error, connection between Client and Server failed\n");
+		free(pClientInfo);
+		close(clientSocket);
+		exit(1);
+	}
 
+	// recv(socket of client, message storage, maximum size of storage, flat);
+	error = recv(clientSocket, buffer, MAXBYTES, 0); // Receives message from Server 
+	if (error == -1)
+	{
+		printf("Error on recv() function\n");
+		free(pClientInfo);
+		close(clientSocket);
+		exit(1);
+	}
+	else if (error == 0)
+	{
+		printf("Error, Server has closed connection\n");
+		free(pClientInfo);
+		close(clientSocket);
+		exit(1);
+	}
+	else
+	{
+		buffer[MAXBYTES] = '\0';
+		printf("Server sent: '%s'\n", buffer);
+	}
 
-
-	
 
 	free(pClientInfo); // Frees the Linked List
-
+	close(clientSocket);
+	return 0;
 }
 
